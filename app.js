eba9ce92948402b523dcebd869ef40efb917fee6
Aviv Biton <avivbiton01@gmail.com>
2019-01-22 21:04:07 +0200

    small fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
